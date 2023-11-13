81e57be1012fea72666f0ca04d59f6cf46669ef8
wezer-pixel <jerryjuma104@gmail.com>
2023-11-13 14:10:25 +0300

    [BUG FIX]PEP8 Validation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Returns the total number of subscribers."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/keen-inscriber)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
