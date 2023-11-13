32eb019f0fab73793dc8f8b3d48e1584f0725a45
wezer-pixel <jerryjuma104@gmail.com>
2023-11-13 10:32:19 +0300

    Strace and puppet debugging Apache

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,20 @@
-# using puppet and strace to find out why apache returns an error
+exec { 'strace_apache':
+	command => 'strace -p $(pgrep apache2) -o /tmp/strace_output.txt -s 200',
+	path	=> '/bin:/usr/bin',
+	creates	=> '/tmp/strace_output.txt',
+	require	=> Package['strace'],
+}
 
-exec { 'fix_500_error':
-	command => "/bin/sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+exec { 'fix_500_e':
+	command	=> "/bin/sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+	path	=> '/bin:/usr/bin',
+	onlyif	=> 'grep -q '.phpp' /var/www/html/wp-settings.php',
+	require	=> Exec['strace_apache2'],
+	notifies => Service['apache2'],
+}
+
+service { 'apache2':
+	ensure	=> running,
+	enable	=> true,
+	require	=> Exec['fix_500_e'],
 }
