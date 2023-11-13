119979a0da2ab3178018d5a10e4c6435e974da7e
wezer-pixel <jerryjuma104@gmail.com>
2023-11-13 14:08:33 +0300

    [TASK]print 10 hot posts by a subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Print first 10 hot posts on a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """Print titles of 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/keen-inscriber)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
